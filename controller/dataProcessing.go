6f95bef35f04e9386b1176091150df3abbfc79f8
Heath000 <2778026361@qq.com>
2024-10-30 12:08:43 +0800

    nothing

--- controller/dataProcessing.go
+++ controller/dataProcessing.go
@@ -5,42 +5,44 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
 type DataProcessingController struct{}
 
-func (ctrl *DataProcessingController) DataStandalize(c *gin.Context) {//数据标准化
+func (ctrl *DataProcessingController) DataStandalize(c *gin.Context) { //数据标准化
 	c.JSON(http.StatusOK, gin.H{
 		"message": "success",
 		"data":    "test",
 	})
 }
-func (ctrl *DataProcessingController) DataOutliersHandle(c *gin.Context) {//异常值处理生成
+func (ctrl *DataProcessingController) DataOutliersHandle(c *gin.Context) { //异常值处理
 	c.JSON(http.StatusOK, gin.H{
 		"message": "success",
 		"data":    "test",
 	})
 }
-func (ctrl *DataProcessingController) DataMissingValuesHandle(c *gin.Context) {//异常值处理生成
+func (ctrl *DataProcessingController) DataMissingValuesHandle(c *gin.Context) { //缺失值处理
 	c.JSON(http.StatusOK, gin.H{
 		"message": "success",
 		"data":    "test",
 	})
 }
-func (ctrl *DataProcessingController) DataFeatureCorrelation(c *gin.Context) {//异常值处理生成
+func (ctrl *DataProcessingController) DataFeatureCorrelation(c *gin.Context) { //相关系数
 	c.JSON(http.StatusOK, gin.H{
 		"message": "success",
 		"data":    "test",
 	})
 }
-func (ctrl *DataProcessingController) DataFeatureVariance(c *gin.Context) {//异常值处理生成
+func (ctrl *DataProcessingController) DataFeatureVariance(c *gin.Context) { //方差
 	c.JSON(http.StatusOK, gin.H{
 		"message": "success",
 		"data":    "test",
 	})
 }
-func (ctrl *DataProcessingController) DataFeatureChiSquare(c *gin.Context) {//异常值处理生成
+func (ctrl *DataProcessingController) DataFeatureChiSquare(c *gin.Context) { //卡方
 	c.JSON(http.StatusOK, gin.H{
 		"message": "success",
 		"data":    "test",
 	})
 }
-//还有缺失值处理	特征筛选-相关系数法、方差选择法、卡方检验法	 
+
+//还有缺失值处理	特征筛选-相关系数法、方差选择法、卡方检验法
